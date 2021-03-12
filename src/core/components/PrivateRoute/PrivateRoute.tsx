0933c6533aa6806b4523b3190b51cc4f138bcb79
ruslannovik <novik.rv93@gmail.com>
2021-03-12 07:41:54 +0300

    fix: fix route of PrivateRoute

--- src/core/components/PrivateRoute/PrivateRoute.tsx
+++ src/core/components/PrivateRoute/PrivateRoute.tsx
@@ -1,20 +1,26 @@
 import { Route } from 'react-router-dom';
-import { RouteProps } from 'react-router';
+import { Redirect, RouteProps } from 'react-router';
 import { connect } from 'react-redux';
-import Loader from '../Loader/Loader';
+import { User } from '../../actions/auth.actions';
+import { routeConstants } from '../../constants/route.constants';
 
 export interface IPrivateRouteProps {
   component: () => JSX.Element;
-  loading: boolean;
+  user: User;
 }
 
-function PrivateRoute({ loading, component: Component, ...rest }: IPrivateRouteProps & RouteProps): JSX.Element {
-  return <Route {...rest} render={(props) => (loading ? <Loader /> : <Component {...props} />)}></Route>;
+function PrivateRoute({ user, component: Component, ...rest }: IPrivateRouteProps & RouteProps): JSX.Element {
+  return (
+    <Route
+      {...rest}
+      render={(props) => (user ? <Component {...props} /> : <Redirect to={routeConstants.SIGNIN} />)}
+    ></Route>
+  );
 }
 
 function mapStateToProps(store: any) {
-  const { loading } = store.authReducer;
-  return { loading };
+  const { user } = store.authReducer;
+  return { user };
 }
 
 export default connect(mapStateToProps)(PrivateRoute);
