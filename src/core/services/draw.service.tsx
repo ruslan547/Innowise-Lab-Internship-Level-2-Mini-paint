706d9e44a5a742050962b0f5e678542543dc6709
ruslannovik <novik.rv93@gmail.com>
2021-03-15 23:35:02 +0300

    feat: add draw.service

--- src/core/services/draw.service.tsx
+++ src/core/services/draw.service.tsx
@@ -1,6 +1,34 @@
 import { MouseEvent } from 'react';
 
-export function drawByPaintbrush(event: EventTarget): void {
-  // const { target } = event;
-  // console.log(event);
+const clickX: any[] = [];
+const clickY: any[] = [];
+const clickDrag: any[] = [];
+
+export function drawByPaintbrush(context: CanvasRenderingContext2D | null): void {
+  if (context) {
+    // context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+    context.strokeStyle = '#df4b26';
+    context.lineJoin = 'round';
+    context.lineWidth = 5;
+
+    clickX.forEach((_, index) => {
+      context?.beginPath();
+      if (clickDrag[index] && index) {
+        context?.moveTo(clickX[index - 1], clickY[index - 1]);
+      } else {
+        context?.moveTo(clickX[index] - 1, clickY[index]);
+      }
+
+      context?.lineTo(clickX[index], clickY[index]);
+      context?.closePath();
+      context?.stroke();
+    });
+  }
+}
+
+export function addClick(x: any, y: any, dragging?: any) {
+  clickX.push(x);
+  clickY.push(y);
+  clickDrag.push(dragging);
 }
--- src/pages/Paint/Paint.tsx
+++ src/pages/Paint/Paint.tsx
@@ -5,10 +5,9 @@ import { paintbrush, startDraw, stopDraw } from '../../core/actions/draw.actions
 import PaintButton from '../../core/components/PaintButton/PaintButton';
 import { drawConstants } from '../../core/constants/draw.constants';
 import { RootSate } from '../../core/reducers/root.reducer';
-import { drawByPaintbrush } from '../../core/services/draw.service';
+import { addClick, drawByPaintbrush } from '../../core/services/draw.service';
 import './Paint.scss';
 import drawbrush from '../../assets/img/drawbrush.svg';
-console.log(drawbrush);
 
 const { PAINTBRUSH } = drawConstants;
 
@@ -19,84 +18,63 @@ export interface PaintProps {
 }
 
 function Paint({ tool, isDraw, dispatch }: PaintProps): JSX.Element {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  // let ctx: CanvasRenderingContext2D | null = null;
-  let context: CanvasRenderingContext2D | null = null;
-  const canvas = useRef<HTMLCanvasElement | null>(null);
-  let mouseX;
-  let mouseY;
-  let paint: boolean;
-  const clickX: any[] = [];
-  const clickY: any[] = [];
-  const clickDrag: any[] = [];
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const context = useRef<CanvasRenderingContext2D | null>(null);
 
-  function addClick(x: any, y: any, dragging?: any) {
-    clickX.push(x);
-    clickY.push(y);
-    clickDrag.push(dragging);
-  }
+  const handlePaintbtushClick = () => {
+    dispatch(paintbrush());
+  };
 
-  function redraw() {
-    if (context) {
-      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-
-      context.strokeStyle = '#df4b26';
-      context.lineJoin = 'round';
-      context.lineWidth = 5;
-
-      clickX.forEach((_, index) => {
-        context?.beginPath();
-        if (clickDrag[index] && index) {
-          context?.moveTo(clickX[index - 1], clickY[index - 1]);
-        } else {
-          context?.moveTo(clickX[index] - 1, clickY[index]);
-        }
-
-        context?.lineTo(clickX[index], clickY[index]);
-        context?.closePath();
-        context?.stroke();
-      });
+  const handleMouseDown = (event: MouseEvent) => {
+    dispatch(startDraw());
+    if (canvasRef && canvasRef.current) {
+      const { offsetLeft, offsetTop } = canvasRef.current;
+      addClick(event.pageX - offsetLeft, event.pageY - offsetTop);
+      drawByPaintbrush(context.current);
     }
-  }
+  };
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (isDraw && tool === PAINTBRUSH) {
+      if (canvasRef && canvasRef.current) {
+        const { offsetLeft, offsetTop } = canvasRef.current;
+        addClick(event.pageX - offsetLeft, event.pageY - offsetTop, true);
+        drawByPaintbrush(context.current);
+      }
+    }
+  };
+
+  const handleMouseUp = () => {
+    dispatch(stopDraw());
+  };
+
+  const handleMouseLeave = () => {
+    dispatch(stopDraw());
+  };
 
   useEffect(() => {
-    canvas.current = document.querySelector('canvas');
-    if (canvas && canvas.current) {
-      context = canvas.current.getContext('2d');
-
-      canvas.current.addEventListener('mousedown', (e) => {
-        if (canvas && canvas.current) {
-          mouseX = e.pageX - canvas.current.offsetLeft;
-          mouseY = e.pageY - canvas.current.offsetTop;
-          paint = true;
-          addClick(e.pageX - canvas.current.offsetLeft, e.pageY - canvas.current.offsetTop);
-          redraw();
-        }
-      });
-
-      canvas.current.addEventListener('mousemove', (e) => {
-        if (paint) {
-          if (canvas && canvas.current) {
-            addClick(e.pageX - canvas.current.offsetLeft, e.pageY - canvas.current.offsetTop, true);
-            redraw();
-          }
-        }
-      });
-
-      canvas.current.addEventListener('mouseup', () => {
-        paint = false;
-      });
+    if (canvasRef && canvasRef.current) {
+      context.current = canvasRef.current.getContext('2d');
     }
   });
 
   return (
     <div className="paint">
-      <canvas className="mainview" width="712px" height="632px"></canvas>
+      <canvas
+        className="mainview"
+        ref={canvasRef}
+        width="712px"
+        height="632px"
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseLeave}
+      ></canvas>
       <div className="toolbar">
         <PaintButton />
         <div className="toolbar__draw">
           <div className="paintbrush">
-            <PaintButton value={drawbrush} />
+            <PaintButton value={drawbrush} onClick={handlePaintbtushClick} />
             <div className="paintbrush__setting"></div>
           </div>
         </div>
